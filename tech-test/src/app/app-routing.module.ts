9ecc00711a8c30e1da32f175a34b7ab90907a121
Patrick <pc8229@exeter.ac.uk>
2019-11-25 10:54:31 +0000

    Route to todo list component

--- tech-test/src/app/app-routing.module.ts
+++ tech-test/src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { TodoListComponent } from './todo-list/todo-list.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'tasks', component: TodoListComponent},
+  {path: '**', component: TodoListComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
